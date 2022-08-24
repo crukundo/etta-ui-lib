f682db5a14190838d76169ffc39c4c1d09e47b4a
Collin Rukundo <collin@matchstick.ug>
2022-08-24 16:40:02 +0300

    Finalized stories and test for Bitcoin Icons

--- packages/showcase-app/storybook/stories/Icon/Icon.stories.tsx
+++ packages/showcase-app/storybook/stories/Icon/Icon.stories.tsx
@@ -1,11 +1,24 @@
 import { storiesOf } from '@storybook/react-native';
 import React from 'react';
-import { BitcoinCircle } from '@ettawallet/rn-bitcoin-icons/dist/filled';
+import { BitcoinCircle, Block } from '@ettawallet/rn-bitcoin-icons/dist/filled';
+import {
+  BitcoinCircle as BitcoinCircleOutline,
+  Block as BlockOutline,
+} from '@ettawallet/rn-bitcoin-icons/dist/outline';
 import { ArtBoard } from '../ArtBoard';
 
 storiesOf('Icon', module)
   .addDecorator(getStory => <ArtBoard>{getStory()}</ArtBoard>)
 
   .add('Filled', () => (
-    <BitcoinCircle width={200} height={200} color="#000000" />
+    <>
+      <BitcoinCircle width={200} height={200} color="#000000" />
+      <Block width={200} height={200} color="#000000" />
+    </>
+  ))
+  .add('Outline', () => (
+    <>
+      <BitcoinCircleOutline width={200} height={200} color="#000000" />
+      <BlockOutline width={200} height={200} color="#000000" />
+    </>
   ));
